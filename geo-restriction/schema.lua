ac3b067f5e2386769ea2a78e90c97eb2b8c3c74e
Alexander Kotelnikov <a.kotelnikov@xsolla.com>
2023-07-23 16:08:57 +0300

    plugin: clarified types in the schema

--- geo-restriction/schema.lua
+++ geo-restriction/schema.lua
@@ -1,29 +1,39 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+local PLUGIN_NAME = "geo-restriction"
+
 return {
-  name = "geo-restriction",
+  name = PLUGIN_NAME,
   fields = {
     {
+      protocols = typedefs.protocols {
+        default = {"http", "https", "tcp", "tls", "grpc", "grpcs"}
+      }
+    }, {
       config = {
         type = "record",
         fields = {
           {
             allow = {
-              description = "List of countries to allow. One of `config.whitelist_countries` or `config.blacklist_countries` must be specified.",
-              type = "array"
+              -- description = "List of countries to allow. One of `config.whitelist_countries` or `config.blacklist_countries` must be specified.",
+              type = "set",
+              elements = {type = "string"}
             }
           }, {
             deny = {
-              description = "List of countries to deny. One of `config.whitelist_countries` or `config.blacklist_countries` must be specified.",
-              type = "array"
+              -- description = "List of countries to deny. One of `config.whitelist_countries` or `config.blacklist_countries` must be specified.",
+              type = "set",
+              elements = {type = "string"}
             }
           }, {
             status = {
-              description = "The HTTP status of the requests that will be rejected by the plugin.",
+              -- description = "The HTTP status of the requests that will be rejected by the plugin.",
               type = "number",
               required = false
             }
           }, {
             message = {
-              description = "The message to send as a response body to rejected requests.",
+              -- description = "The message to send as a response body to rejected requests.",
               type = "string",
               required = false
             }
